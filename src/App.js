014549d43ed191271a499f0079d708d127c89c94
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2022-02-07 14:13:31 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useContext, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainLoader from './components/layouts/partials/mainLoader'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// files
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+//components
+const NotFound = React.lazy(() => import('./components/public/404'));
+const NewToken = React.lazy(() => import('./components/public/Home'));
+const RoadMap = React.lazy(() => import('./components/public/RoadMap'));
+const TokenPage = React.lazy(() => import('./components/public/Token'));
+// const TokenPage = 
+
+const App = () => {
+
+    
+
+  return(
+    
+    <Router>
+
+      <Suspense fallback={MainLoader()}>
+          <Switch>
+            <Route exact path='/' component={NewToken} />
+            <Route exact path='/roadmap' component={RoadMap} />
+            <Route exact path='/token' component={TokenPage} />
+            
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+      </Suspense>
+
+    </Router>
+
+  )
 }
 
 export default App;
