d6a3b73fb7f05bc1085591fbdc514513c1451a23
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2022-02-08 08:35:18 +0100

    ----

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import React, { Suspense, useContext, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MainLoader from './components/layouts/partials/mainLoader'
-
-// files
+import logo from './logo.svg';
 import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
 
-
-
-//components
-const NotFound = React.lazy(() => import('./components/public/404'));
-const NewToken = React.lazy(() => import('./components/public/Home'));
-const RoadMap = React.lazy(() => import('./components/public/RoadMap'));
-const TokenPage = React.lazy(() => import('./components/public/Token'));
-// const TokenPage = 
-
-const App = () => {
-
-    
-
-  return(
-    
-    <Router>
-
-      <Suspense fallback={MainLoader()}>
-          <Switch>
-            <Route exact path='/' component={NewToken} />
-            <Route exact path='/roadmap' component={RoadMap} />
-            <Route exact path='/token' component={TokenPage} />
-            
-            <Route exact path="*" component={NotFound} />
-          </Switch>
-      </Suspense>
-
-    </Router>
-
-  )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React!</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
